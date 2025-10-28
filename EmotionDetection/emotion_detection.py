4d354d7484befd1b02548262e291c2bc13eb6c37
Katherine <katherineoyeri@googlemail.com>
2025-10-28 13:56:29 +0100

    Refactor emotion detection logic for clarity

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,36 +1,39 @@
-import requests
 import json
+import requests
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
-    response = requests.post(url, json=myobj, headers=header)
 
-    #convert from str to dict
-    formatted_response = response.json()
+    response = requests.post(url, json=myobj, headers=header)
     
-    #index emotion from list
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    
+    formatted_response = json.loads(response.text)
     emotion = formatted_response['emotionPredictions'][0]['emotion']
 
-    #extract individual emotions
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    anger = emotion['anger']
+    disgust = emotion['disgust']
+    fear = emotion['fear']
+    joy = emotion['joy']
+    sadness = emotion['sadness']
 
-    #find max emotion
-    dom_emotion = max(emotion, key=emotion.get)
+    dominant_emotion = max(emotion, key=emotion.get)
 
-    #setup emotion dict
-    emotion_dict = {
+    return {
         'anger': anger,
         'disgust': disgust,
         'fear': fear,
         'joy': joy,
         'sadness': sadness,
-        'dominant_emotion': dom_emotion
+        'dominant_emotion': dominant_emotion
     }
-    return emotion_dict
